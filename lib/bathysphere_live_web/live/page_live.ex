22bf25e20291d8e643388fd66191d3d00e688429
Fatty <fatty.arbuckle.87@gmail.com>
2020-05-27 09:22:27 -0400

    Supports selecting actions

--- lib/bathysphere_live_web/live/page_live.ex
+++ lib/bathysphere_live_web/live/page_live.ex
@@ -51,6 +51,52 @@ defmodule BathysphereLiveWeb.PageLive do
     </section>
     """
   end
+  # {{:stress, -1, false}, 0}, {{:oxygen, -1, false}, 1}
+  def render(%{game_state: %{state: {:select_action, choices}}} = assigns) do
+    ~L"""
+    <section class="hero">
+      <div class="hero-body">
+        <div class="modal is-active">
+          <div class="modal-background"></div>
+          <div class="modal-content">
+            <h1>You have a choice to make!</h1>
+            <%= for {{resource, cost, used?}, index} <- choices do %>
+              <button
+                class="button"
+                phx-click="select-option"
+                phx-value-resource="<%= resource %>"
+                phx-value-cost="<%= cost %>"
+                phx-value-used="<%= used? %>"
+                phx-value-index="<%= index %>"
+              >
+                <%= resource %> <%= cost %>
+              </button>
+            <% end %>
+          </div>
+        </div>
+
+        <button class="button" phx-click="reset-game">RESET</button>
+        <div>
+          <%= live_component(
+            @socket,
+            BathysphereLiveWeb.GameComponent,
+            game_state: @game_state)
+          %>
+        </div>
+      </div>
+    </section>
+    """
+  end
+  def render(%{game_state: whatever} = assigns) do
+    IO.inspect(whatever, label: "WHAT?!")
+    ~L"""
+    <section class="hero">
+      <div class="hero-body">
+        <h1>Whatever!</h1>
+      </div>
+    </section>
+    """
+  end
 
   @impl true
   def handle_event("select-map", %{ "map-name" => name }, socket) do
@@ -87,6 +133,19 @@ defmodule BathysphereLiveWeb.PageLive do
     {die, _} = Integer.parse(value)
     {idx, _} = Integer.parse(index)
     move(die, idx, direction)
+    {_state, game_state} = BathysphereLive.Backend.Game.state()
+    {
+      :noreply,
+      assign(
+        socket,
+        game_state: game_state
+      )
+    }
+  end
+
+  def handle_event("select-option", %{ "resource" => resource, "cost" => cost, "used" => used?, "index" => index }, socket) do
+    {value, _} = Integer.parse(cost)
+    BathysphereLive.Backend.Game.select_action({{String.to_atom(resource), value, false}, index})
     {_state, game_state} = BathysphereLive.Backend.Game.state()
     {
       :noreply,
