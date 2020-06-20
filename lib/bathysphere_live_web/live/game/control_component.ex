4c310ca52db9d41a0e6cc1e1d4873af464712b00
Fatty <fatty.arbuckle.87@gmail.com>
2020-06-20 19:15:14 -0400

    Design changes to control panel

--- lib/bathysphere_live_web/live/game/control_component.ex
+++ lib/bathysphere_live_web/live/game/control_component.ex
@@ -4,11 +4,22 @@ defmodule BathysphereLiveWeb.Game.ControlComponent do
   def render(assigns) do
     ~L"""
       <article class="message is-primary">
-        <div class="message-header">
-          <span class="is-size-3">
-           Control Panel
-          </span>
-          <span class="control">
+          <%= live_component(@socket, BathysphereLiveWeb.Game.Control.Score,
+            score: @score,
+            fish_points: @fish_points,
+            fish_count: @fish_count,
+            octopus_points: @octopus_points,
+            octopus_count: @octopus_count
+          ) %>
+      </article>
+
+      <article class="message is-primary">
+        <div class="box">
+          <%= live_component(@socket, BathysphereLiveWeb.Game.Control.Resources, resources: @resources) %>
+        </div>
+      </article>
+
+      <article class="message is-primary">
             <div
               class="button is-text"
               has-tooltip-multiline
@@ -19,25 +30,45 @@ defmodule BathysphereLiveWeb.Game.ControlComponent do
                 <i class="fas fa-power-off"></i>
               </span>
             </div>
-          </span>
-
-        </div>
-        <div class="message-body has-text-left">
-
-          <%= live_component(@socket, BathysphereLiveWeb.Game.Control.Score,
-            score: @score,
-            fish_points: @fish_points,
-            fish_count: @fish_count,
-            octopus_points: @octopus_points,
-            octopus_count: @octopus_count
-          ) %>
-
-          <hr/>
-
-          <%= live_component(@socket, BathysphereLiveWeb.Game.Control.Resources, resources: @resources) %>
-        </div>
       </article>
+
     """
   end
+
+  # <article class="message is-primary">
+  #   <div class="message-header">
+  #     <span class="is-size-3">
+  #      Control Panel
+  #     </span>
+  #     <span class="control">
+  #       <div
+  #         class="button is-text"
+  #         has-tooltip-multiline
+  #         data-tooltip="Abandon the game"
+  #         phx-click="reset-game"
+  #       >
+  #         <span class="is-size-1 has-text-danger">
+  #           <i class="fas fa-power-off"></i>
+  #         </span>
+  #       </div>
+  #     </span>
+  #
+  #   </div>
+  #   <div class="message-body has-text-left">
+  #
+  #     <%= live_component(@socket, BathysphereLiveWeb.Game.Control.Score,
+  #       score: @score,
+  #       fish_points: @fish_points,
+  #       fish_count: @fish_count,
+  #       octopus_points: @octopus_points,
+  #       octopus_count: @octopus_count
+  #     ) %>
+  #
+  #     <hr/>
+  #
+  #     <%= live_component(@socket, BathysphereLiveWeb.Game.Control.Resources, resources: @resources) %>
+  #   </div>
+  # </article>
+
 
 end
